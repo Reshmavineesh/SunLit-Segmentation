e46792e286e19268c03a2f4724d697a454890ddc
Reshmavineesh <reshmalkofficial@gmail.com>
2024-03-24 22:36:03 +0530

    Chore: restore downloads

--- download_dataset.sh
+++ download_dataset.sh
@@ -22,22 +22,22 @@ fi
 rm -rf dataset; mkdir dataset; cd dataset
 
 # Download from kaggle and unzip
-# kaggle datasets download -d pvtsec0x1/sunlit-pistachio &
-# kaggle datasets download -d pvtsec0x1/sunlit-tomato &
+kaggle datasets download -d pvtsec0x1/sunlit-pistachio &
+kaggle datasets download -d pvtsec0x1/sunlit-tomato &
 kaggle datasets download -d pvtsec0x1/sunlit-chilly &
 kaggle datasets download -d pvtsec0x1/sunlit-chilly-128 &
 wait
-# unzip -q sunlit-pistachio.zip -d ./dataset_pistachio &
-# unzip -q sunlit-tomato.zip -d ./dataset_tomato &
+unzip -q sunlit-pistachio.zip -d ./dataset_pistachio &
+unzip -q sunlit-tomato.zip -d ./dataset_tomato &
 unzip -q sunlit-chilly.zip -d ./dataset_chilly &
 unzip -q sunlit-chilly-128.zip -d ./dataset_chilly_128 &
 wait
 
-# kaggle datasets download -d pvtsec0x1/pistachio-leaves &
-# kaggle datasets download -d pvtsec0x1/tomato-leaves &
-# wait
-# unzip -q pistachio-leaves.zip -d ./dataset_pistachio_128 &
-# unzip -q tomato-leaves.zip -d ./dataset_tomato_128 &
-# wait
+kaggle datasets download -d pvtsec0x1/pistachio-leaves &
+kaggle datasets download -d pvtsec0x1/tomato-leaves &
+wait
+unzip -q pistachio-leaves.zip -d ./dataset_pistachio_128 &
+unzip -q tomato-leaves.zip -d ./dataset_tomato_128 &
+wait
 
 rm -rf *.zip
